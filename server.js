926e555f1e1b73e0ee226c67526d5576ed8377b1
tnvrgit <37243335+tnvrgit@users.noreply.github.com>
2020-08-06 20:47:27 +0530

    Added express in server.js

--- server.js
+++ server.js
@@ -1,16 +1,7 @@
-var http = require('http');
-var fs = require('fs');
+var express = require('express');
+var app = express();
 
-const PORT=80; 
+app.use(express.static('public'));
+app.use(express.static('images'));
 
-fs.readFile('./index.html', function (err, html) {
-
-    if (err) throw err;    
-
-    http.createServer(function(request, response) {  
-        response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(html);  
-        response.end();  
-    }).listen(PORT);
-    console.log('listening on port 8080')
-});
+app.listen(80);
